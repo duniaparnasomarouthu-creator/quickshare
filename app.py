e0e21cfe47532a71a72a7c78d7d4d63be950ed4a
duniaparnasomarouthu-creator <duniaparnasomarouthu@gmail.com>
2026-04-26 22:15:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+# Create folder if not exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/upload", methods=["POST"])
 def upload():
+    # FIX: prevents Bad Request error
+    if "file" not in request.files:
+        return "No file part selected"
+
     file = request.files["file"]
+
+    if file.filename == "":
+        return "No file selected"
 
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
     file.save(filepath)
