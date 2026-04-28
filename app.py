314a86d5c5c2a68b3866a024fb4aa81aea4893e4
duniaparnasomarouthu-creator <duniaparnasomarouthu@gmail.com>
2026-04-28 21:10:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ def admin():
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
