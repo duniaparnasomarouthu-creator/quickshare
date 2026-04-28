c0d27b68554c4142ecce93d58ed52c50a90b37fa
duniaparnasomarouthu-creator <duniaparnasomarouthu@gmail.com>
2026-04-28 20:00:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,5 +222,8 @@ def admin():
                            percent=percent)
 
 # -------- RUN --------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
